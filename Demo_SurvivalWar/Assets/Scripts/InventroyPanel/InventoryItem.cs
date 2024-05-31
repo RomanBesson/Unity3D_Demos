7c0de40ed5569e78f083e11bdd7e1a9e89a351eb
RomanBesson <1350648257@qq.com>
2024-05-31 14:20:35 +0800

    2024_05_31 13:51 生存之战开始

---
+++ Demo_SurvivalWar/Assets/Scripts/InventroyPanel/InventoryItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 背包物品的数据实体类.
+/// </summary>
+public class InventoryItem
+{
+
+    private string itemName;
+    private int itemNum;
+
+    public string ItemName
+    {
+        get { return itemName; }
+        set { itemName = value; }
+    }
+
+    public int ItemNum
+    {
+        get { return itemNum; }
+        set { itemNum = value; }
+    }
+
+    public InventoryItem() { }
+    public InventoryItem(string itemName, int itemNum)
+    {
+        this.ItemName = itemName;
+        this.ItemNum = itemNum;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("物品的名称:{0}, 数量:{1}", this.itemName, this.itemNum);
+    }
+}
---
+++ Demo_SurvivalWar/Assets/Scripts/InventroyPanel/InventoryItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemController : MonoBehaviour
+{
+    private Image m_Image; //物品图片
+    private Text m_Text; //物品数量
+
+    private void Awake()
+    {
+        m_Image = gameObject.GetComponent<Image>();
+        m_Text = transform.Find("Num").GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// 初始化背包物品信息
+    /// </summary>
+    /// <param name="name">物品图片名称</param>
+    /// <param name="num">物品个数</param>
+    public void InitInitItem(string name, int num)
+    {
+        m_Image.sprite = Resources.Load<Sprite>("Item/" + name);
+        m_Text.text = num.ToString();
+    }
+}
